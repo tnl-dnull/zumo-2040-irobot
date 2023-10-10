b43808f0b9124d2e68d697c440cdc3211444bf59
Kevin (Pololu) <dev-kevin@pololu.com>
2023-10-09 21:09:19 -0700

    proximity_sensors: moved out of ir_sensors to separate module

--- micropython_demo/zumo_2040_robot/robot.py
+++ micropython_demo/zumo_2040_robot/robot.py
@@ -4,7 +4,8 @@ from .buzzer import Buzzer
 from .display import Display
 from .encoders import Encoders
 from .imu import IMU
-from .ir_sensors import LineSensors, ProximitySensors
+from .ir_sensors import LineSensors
 from .motors import Motors
+from .proximity_sensors import ProximitySensors
 from .rgb_leds import RGBLEDs
 from .yellow_led import YellowLED
