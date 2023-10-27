8a14722e599d27c499a09e172aed8f6b57e95959
Kevin (Pololu) <dev-kevin@pololu.com>
2023-10-27 15:34:13 -0700

    self_test updated for Zumo

--- micropython_demo/self_test.py
+++ micropython_demo/self_test.py
@@ -1,19 +1,24 @@
 import machine
+from machine import Pin
 from zumo_2040_robot import robot
 import sys
 import time
+from math import pi
 
 battery = robot.Battery()
-bump_sensors = robot.BumpSensors()
 button_a = robot.ButtonA()
 button_b = robot.ButtonB()
 button_c = robot.ButtonC()
 buzzer = robot.Buzzer()
 display = robot.Display()
 encoders = robot.Encoders()
-imu = robot.IMU()
 motors = robot.Motors()
 rgb_leds = robot.RGBLEDs()
+
+# initialize these later after testing front pins:
+imu = None
+line_sensors = None
+proximity_sensors = None
 
 BEEP_WELCOME = '!>g32>>c32'
 BEEP_FAIL = '<g-8r8<g-8r8<g-8'
@@ -27,8 +32,8 @@ def display_line_break(lines=1, show=True):
     display.fill_rect(0, 64 - 8 * lines, 128, 8 * lines, 0)
     if show: display.show()
 
-def display_centered_text(text, show=True):
-    display.text(text, (128 - len(text) * 8) // 2, 56)
+def display_centered_text(text, y=56, show=True):
+    display.text(text, (128 - len(text) * 8) // 2, y)
     if show: display.show()
 
 # Custom exception for test errors
@@ -47,6 +52,139 @@ def show_test_error(test_error):
     rgb_leds.show()
 
     buzzer.play_in_background(BEEP_FAIL)
+
+IO_BANK0_BASE = const(0x40014000)
+
+# reads a bit from GPIOx_STATUS register
+def gpio_status_helper(pin, bit):
+    if pin > 29: raise ValueError('invalid pin')
+    return bool(machine.mem32[IO_BANK0_BASE + 8 * pin] >> bit & 1)
+
+# reads GPIOx_STATUS.OETOPAD
+def pin_is_output(pin):
+    return gpio_status_helper(pin, 13)
+
+# reads GPIOx_STATUS.OETOPAD
+def pin_output_is_high(pin):
+    # GPIOx_STATUS.OUTTOPAD
+    return gpio_status_helper(pin, 9)
+
+# Computes the expected state of a pin, considering whether and how it's driving
+# and a given default state otherwise (usually determined by a pull-up or
+# pull-down). If the pin is None, the expected state is the same as the default
+# state. The default state can be None, which represents an indeterminate state
+# when not being driven (e.g. encoder pins, which can be driven low but
+# otherwise might be either high or low depending on the encoder position)
+def expected_state(pin, default_state):
+    if pin is None:
+        return default_state
+    else:
+        return pin_output_is_high(pin) if pin_is_output(pin) else default_state
+
+def check_input(pin, default_state):
+    expected = expected_state(pin, default_state)
+    actual = Pin(pin).value()
+
+    if expected is not None and expected != actual:
+        print(Pin(pin))
+        raise TestError(f"GP{pin}: expected {int(expected)}, got {int(actual)}")
+
+def check_front_inputs():
+    check_input(4, True) # GP4: SDA; pulled up by IMU circuit
+    check_input(5, True) # GP5: SCL; pulled up by IMU circuit
+    check_input(16, False) # GP16: right prox LED ctrl; pulled down
+    check_input(17, False) # GP17: left prox LED ctrl; pulled down
+    check_input(18, False) # GP18: down sensor 5 (R); pulled down
+    check_input(19, False) # GP19: down sensor 4; pulled down
+    check_input(20, False) # GP20: down sensor 3; pulled down
+    check_input(21, False) # GP21: down sensor 2; pulled down
+    check_input(22, False) # GP22: down sensor 1 (L); pulled down
+    # GP23: driven by left prox sensor; unknown state
+    # GP24: driven by right prox sensor; unknown state
+    check_input(26, False) # GP26: down IR LED control & BATLEV divider
+    # GP24: driven by front prox sensor; unknown state
+    check_input(28, False) # GP28: free; internal pull-down enabled
+    check_input(29, False) # GP29: free; internal pull-down enabled
+
+def check_driving_high_and_low(pin):
+    p = Pin(pin, mode=Pin.OUT, value=1)
+    check_front_inputs()
+    p.value(0)
+    check_front_inputs()
+    p.init(mode=Pin.IN)
+
+def test_front_pins():
+    Pin(28, mode=Pin.IN, pull=Pin.PULL_DOWN)
+    Pin(29, mode=Pin.IN, pull=Pin.PULL_DOWN)
+    time.sleep_ms(1)
+
+    check_front_inputs()
+
+    for i in [4, 5, 16, 17, 18, 19, 20, 21, 22, 26, 28, 29]:
+        check_driving_high_and_low(i)
+
+def test_proximity_sensors():
+    proximity_sensors = robot.ProximitySensors()
+
+    display.text('Left Front Right', 0, 40)
+    display.text('Far   Far   Far ', 0, 48)
+    display.text('Near  Near  Near', 0, 56)
+    display.show()
+    tested = 0
+
+    count_funcs = [
+        proximity_sensors.left_counts_with_left_leds,
+        proximity_sensors.front_counts_with_left_leds,
+        proximity_sensors.right_counts_with_right_leds]
+    notes = ['c', 'd', 'e']
+
+    while True:
+        proximity_sensors.read()
+
+        for i in range(3):
+            if not tested & (1 << i*2) and count_funcs[i]() >= 5:
+                tested |= (1 << i*2)
+                display.fill_rect(48*i, 56, 32, 8, 1)
+                display.text('Near', 48*i, 56, 0)
+                display.show()
+                buzzer.play_in_background(f">{notes[i]}32")
+            if not tested & (1 << i*2+1) and count_funcs[i]() <= 1:
+                tested |= (1 << i*2+1)
+                display.fill_rect(48*i, 48, 32, 8, 1)
+                display.text('Far', 48*i, 48, 0)
+                display.show()
+                buzzer.play_in_background(f"{notes[i]}32")
+
+        if tested == 0b111111:
+            return
+
+def test_line_sensors():
+    line_sensors = robot.LineSensors()
+
+    line = line_sensors.read()
+
+    for i in range(5):
+        if line[i] > 900:
+            display_line_break()
+            display_centered_text(f"Emitters on:")
+            for j in range(5):
+                display_line_break()
+                display_centered_text(f"{j}: {line[j]}")
+            print(line)
+            raise TestError(f"Line sensor {i} (emitters on): expected <= 900, read {line[i]}")
+
+    line_sensors.start_read(emitters_on=False)
+    line = line_sensors.read()
+
+    for i in range(5):
+        if line[i] < 900:
+            display_line_break()
+            display_centered_text(f"Emitters off:")
+            for j in range(5):
+                display_line_break()
+                display_centered_text(f"{j}: {line[j]}")
+            print(line)
+            raise TestError(f"Line sensor {i} (emitters off): expected >= 900, read {line[i]}")
 
 def run_test():
     rgb_leds.set_brightness(2)
@@ -74,19 +212,37 @@ def run_test():
     display_centered_text(f"VBAT {v:4} mV")
     if v > 7000:
         raise TestError(f"VBAT higher than expected: {v} mV")
+    time.sleep_ms(250)
+
+    display_line_break(2)
+    display_centered_text('Front pins   ')
+    test_front_pins()
+    display_centered_text('           OK')
     rgb_leds.set(5, YELLOW) # F
     rgb_leds.show()
     time.sleep_ms(250)
 
     display_line_break(2)
-    display_centered_text('Select edition')
+    display_centered_text('Proximity')
+    display_line_break(3)
+    test_proximity_sensors()
     display_line_break()
-    display_centered_text('A: Standard')
-    display_line_break()
-    display_centered_text('B: Turtle  ')
-    display_line_break()
-    display_centered_text('C: Hyper   ')
+    display_centered_text('OK')
     rgb_leds.set(4, YELLOW) # E
+    rgb_leds.show()
+    time.sleep_ms(250)
+
+    display_line_break(2)
+    display_centered_text('Set on surface.')
+    display_line_break()
+    display_centered_text('Gear ratio?')
+    display_line_break()
+    display_centered_text('A: 50:1 ')
+    display_line_break()
+    display_centered_text('B: 75:1 ')
+    display_line_break()
+    display_centered_text('C: 100:1')
+    rgb_leds.set(3, YELLOW) # D
     rgb_leds.show()
     buzzer.play_in_background('<g32')
 
@@ -98,84 +254,106 @@ def run_test():
     time.sleep_ms(10)
     while True:
         if button_a.check():
-            edition = 'Standard'
+            gear_ratio = 50
             display.fill_rect(0, 48, 128, 16, 0)
             buzzer.play_in_background('c32')
             break
         elif button_b.check():
-            edition = 'Turtle'
+            gear_ratio = 75
             display.fill_rect(0, 40, 128, 8, 0)
             display.fill_rect(0, 56, 128, 8, 0)
             buzzer.play_in_background('e32')
             break
         elif button_c.check():
-            edition = 'Hyper'
+            gear_ratio = 100
             display.fill_rect(0, 40, 128, 16, 0)
             buzzer.play_in_background('g32')
             break
     display.show()
-    rgb_leds.set(3, YELLOW) # D
-    rgb_leds.show()
 
-    bump_sensors.calibrate()
     display_line_break(2)
-    display_centered_text('Press bumpers')
-
-    while True:
-        bump_sensors.read()
-        if bump_sensors.left_is_pressed() and bump_sensors.right_is_pressed(): break
-
-    display.fill_rect(0, 56, 128, 8, 0)
-    display_centered_text('Bumpers OK')
+    display_centered_text('Line sensors   ')
+    test_line_sensors()
+    display_centered_text('             OK')
     rgb_leds.set(0, YELLOW) # A
     rgb_leds.show()
-    buzzer.play_in_background('>c32')
     time.sleep_ms(250)
 
     # test IMU presence
     display_line_break(2)
     display_centered_text('IMU   ')
+    imu = robot.IMU()
     imu.reset()
     if not imu.detect():
         raise TestError('IMU not detected')
     display_centered_text('    OK')
     rgb_leds.set(1, YELLOW) # B
     rgb_leds.show()
-    time.sleep_ms(250)
+    time.sleep_ms(750)
 
     display_line_break(2)
     display_centered_text('Motors')
     imu.enable_default()
-    imu.gyro.set_output_data_rate(833)
-    imu.gyro.set_full_scale(2000)
+    imu.acc.set_output_data_rate(1666)
+    imu.acc.set_full_scale(4)
 
+    # calibrate accelerometer: find steady-state offset to subtract from
+    # subsequent readings, helping to account for inclined surfaces
+    OFFSET_SAMPLES = const(50)
+    acc_offset = 0
+    for _ in range(OFFSET_SAMPLES):
+        imu.read()
+        acc_offset += imu.acc.last_reading_g[0]
+    acc_offset /= OFFSET_SAMPLES
+
+    dist = 0 # in mm
+    speed = 0 # in m/s
     encoders.get_counts(reset=True)
-    motors.set_speeds(1350, -1350)
-    time.sleep_ms(250)
+    motors.set_speeds(1500, 1500)
+    start = time.ticks_us()
+    prev = start
 
-    imu.gyro.read()
-    gyro_z = imu.gyro.last_reading_dps[2]
+    while dist < 50:
+        imu.read()
+
+        now = time.ticks_us()
+        delta_t_us = time.ticks_diff(now, prev)
+
+        acc = (imu.acc.last_reading_g[0] - acc_offset) * 9.81 # in m/s^2
+        speed += acc * delta_t_us / 1e6 # in m/s
+        dist += speed * delta_t_us / 1e6 * 1000 # in mm
+
+        prev = now
+
+        if time.ticks_diff(now, start) > 1500000: # 1.5 s
+            motors.set_speeds(0, 0)
+            display_line_break()
+            display_centered_text(f"dist={dist:.1f}")
+            raise TestError(f"Timed out trying to drive forward 50 mm: dist={dist:.1f}")
 
     motors.set_speeds(0, 0)
-    time.sleep_ms(100)
     left, right = encoders.get_counts()
-    display_line_break()
+
+    # 12 CPR encoders; 39 mm wheel + track diameter
+    # average left and right encoder counts
+    # counts / 12CPR / ratio * pi * 39mm = dist
+    # ratio = counts / 12CPR * pi * 39mm / dist
+    calc_ratio = (left + right) / 2 / 12 * pi * 39 / dist
+
+    display_line_break(2)
     display_centered_text(f"L={left} R={right}")
     display_line_break()
-    display_centered_text(f"Gyro Z={gyro_z:.1f}")
+    display_centered_text(f"calc ratio={calc_ratio:.1f}")
     display_line_break(2)
-    display_centered_text(f"{edition}:")
-    if edition == 'Standard' and gyro_z > -490 and gyro_z < -350 and \
-            left > 212 and left < 288 and right > -288 and right < -212:
+    display_centered_text(f"{gear_ratio}:1:")
+    if gear_ratio == 50 and calc_ratio > 37.5 and calc_ratio < 62.5:
         pass
-    elif edition == 'Turtle' and gyro_z > -126 and gyro_z < -84 and \
-            left > 140 and left < 200 and right > -200 and right < -140:
+    elif gear_ratio == 75 and calc_ratio > 62.5 and calc_ratio < 87.5:
         pass
-    elif edition == 'Hyper' and gyro_z > 665 and gyro_z < 1190 and \
-            left > 130 and left < 370 and right > -370 and right < -130:
+    elif gear_ratio == 100 and calc_ratio > 87.5 and calc_ratio < 112.5:
         pass
     else:
-        raise TestError(f"Edition mismatch: expected {edition}, measured L={left} R={right} Gyro Z={gyro_z:.1f}")
+        raise TestError(f"Edition mismatch: expected {gear_ratio}:1, measured L={left} R={right} calc ratio={calc_ratio:.1f}")
 
     display_line_break()
     display_centered_text('PASS')
